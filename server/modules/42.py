a2774ec31e8e424ab2bee60363e341c38471e0cc
KlaraKramer <46454343+KlaraKramer@users.noreply.github.com>
2019-05-18 14:01:05 +0200

    Update 42.py

--- server/modules/42.py
+++ server/modules/42.py
@@ -2,9 +2,13 @@ def isValid(text):
     text = text.lower()
     if 'was' in text and 'antwort' in text:
         return True
+    if 'warum' in text:
+        return True
     else:
         return False
 
 def handle(text, tiane, profile):
     if 'antwort' in text.lower():
         tiane.say('42')
+    elif 'warum' in text.lower():
+        tiane.say('Weil Baum')
